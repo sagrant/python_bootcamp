1387327656b1044a39d9b9c3de6865f8c9e6a143
Sadie Grant <75453911+sagrant@users.noreply.github.com>
2025-03-30 13:20:39 -0700

    Add files via upload

---
+++ example.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import pandas as pd
+import numpy as np
+
+in_file = 'alignment_ex.sam'
+
+handle = open(in_file, 'r')
+
+# for line in handle:
+#     print(line)
+
+
+integer_1 = 5
+float_1 = 5.0
+float_2 = 3.5
+string_1 = 'hello world'
+
+bool_1 = True
+bool_2 = False
+
+math_equation_1 = integer_1 + float_1
+
+
+print(math_equation_1)
