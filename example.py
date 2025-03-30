278e30a27dde5ce3c146e11d78b90578c249b09a
Sadie Grant <75453911+sagrant@users.noreply.github.com>
2025-03-30 13:28:45 -0700

    add sections to example.py

--- example.py
+++ example.py
@@ -2,14 +2,7 @@
 import pandas as pd
 import numpy as np
 
-in_file = 'alignment_ex.sam'
-
-handle = open(in_file, 'r')
-
-# for line in handle:
-#     print(line)
-
-
+### DATA TYPES
 integer_1 = 5
 float_1 = 5.0
 float_2 = 3.5
@@ -19,6 +12,24 @@ bool_1 = True
 bool_2 = False
 
 math_equation_1 = integer_1 + float_1
-
-
 print(math_equation_1)
+
+### READING AND WRITING FILES
+# Step 1: Create a variable that stores the input file path 
+in_file = 'alignment_ex.sam'
+
+# Step 2: Create a variable that stores the file handle
+handle = open(in_file, 'r') # Step 3. Open the input file with the built-in function open()
+
+### ITERATORS
+# Step 1: instantiate list 
+lst = [] 
+
+ # Step 2: iterate over file contents
+for line in handle:
+
+  # Step 3: use conditional statements to parse lines of interest from file
+  if not line.startswith('@'):
+    lst.append(line)
+  
+
