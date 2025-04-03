17d9fb5fb25b583c498457804d81ea85efc913d2
Sadie Grant <75453911+sagrant@users.noreply.github.com>
2025-04-03 12:17:44 -0700

    Added pandas section and iterator section. Added documentation

--- example.py
+++ example.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import numpy as np
 
-### DATA TYPES
+### DATA TYPES ###
 integer_1 = 5
 float_1 = 5.0
 float_2 = 3.5
@@ -11,25 +11,49 @@ string_1 = 'hello world'
 bool_1 = True
 bool_2 = False
 
-math_equation_1 = integer_1 + float_1
-print(math_equation_1)
+list_1 = [integer_1, bool_1, string_1, bool_2, float_1]
 
-### READING AND WRITING FILES
+print('this is the entire list = ' + str(list_1))
+print('this is the first item in the list = ' + str(list_1[0]))
+print('this is the second item in the list = ' + str(list_1[1]))
+
+### READING AND WRITING FILES ####
 # Step 1: Create a variable that stores the input file path 
 in_file = 'alignment_ex.sam'
 
 # Step 2: Create a variable that stores the file handle
 handle = open(in_file, 'r') # Step 3. Open the input file with the built-in function open()
 
-### ITERATORS
-# Step 1: instantiate list 
-lst = [] 
+### ITERATORS ###
+# Step 1: instantiate data structure of choice
+test_list = [] # instantiate a list with brackets
+test_dict = {} # instantiate a dictionary with curly brackets
 
- # Step 2: iterate over file contents
+# Step 2: iterate over file contents
 for line in handle:
 
-  # Step 3: use conditional statements to parse lines of interest from file
-  if not line.startswith('@'):
-    lst.append(line)
-  
+    # Step 3: use a conditional statement to skip header lines
+    if not line.startswith('@'): 
+        
+        split_line = line.split('\t') # use tab delimiter to split up lines with data
 
+        sample_ID = split_line[2] # assign variable "sample_ID" 
+        sequence_length = split_line[4] # assign variable "sequence_length"
+        sequence = split_line[9] # assign variable "sequence"
+
+        test_list.append(sample_ID) # put stuff in your list 
+        test_dict[sample_ID] = sequence # put stuff in your dictionary 
+
+# Now you can iterate over your dictionary or list
+for key_sample_ID, value_sequence in test_dict.items():
+    get_sequence = test_dict.get(key_sample_ID)
+
+
+### PANDAS ###
+# Read file in as a dataframe 
+sam_file = pd.read_csv('alignment_ex.sam', sep = '\t', skiprows = 9) 
+
+# Iterate over SAM file dataframe:
+for result, data in sam_file.iterrows():
+    alignment_hit = result[0]
+    data_point = data[0]
